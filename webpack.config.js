eb81a376c45a659d5e9270b014e0f271e436039f
Fábio Vinícius Lima <limavfabio@gmail.com>
2022-08-23 17:44:57 -0300

    Create elements

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,17 @@ module.exports = {
         rules: [
             {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'], // for css
+        use: ['style-loader', 'css-loader', 'postcss-loader'], // for css
+      },
+      {
+        test: /\.js$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
