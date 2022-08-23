48cbf8ad45453ef51891a5fc743f205746781705
Fábio Vinícius Lima <limavfabio@gmail.com>
2022-08-23 18:58:11 -0300

    Generate main dynamically

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './style.css';
+import addScoresContainer from './modules/addScores.js';
+import scoresTable from './modules/scoresTable.js';
+
+document.querySelector('main').innerHTML += scoresTable;
+document.querySelector('main').innerHTML += addScoresContainer;
